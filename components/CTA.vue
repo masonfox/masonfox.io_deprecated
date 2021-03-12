c7fc12d2880826b542f9af530fb80992b46b1a3a
Mason Fox <masonfox22@gmail.com>
2021-03-12 08:10:07 -0500

    tailwind templates to fill out the mock

---
+++ components/CTA.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-green-400">
+        <div class="max-w-2xl mx-auto text-center py-14 px-4">
+            <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+                <span class="block">Boost your productivity.</span>
+                <span class="block">Start using Workflow today.</span>
+            </h2>
+            <p class="mt-4 text-lg leading-6 text-green-700">Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla nec.</p>
+            <a href="#" class="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-green-600 bg-white hover:bg-indigo-50 sm:w-auto">
+            Contact Me
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    
+</style>
